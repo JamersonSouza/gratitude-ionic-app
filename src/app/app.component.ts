59fec7831d00788a2a6e0987ce35132923e7ceba
Jamerson Souza <jamersonsouza@outlook.com>
2026-01-08 21:04:59 -0300

    feat: adição do capacitor para splash inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { SplashScreen } from '@capacitor/splash-screen';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+      this.showSplashScreen();
+  }
+
+
+  async showSplashScreen() {
+    await SplashScreen.show({
+      showDuration: 3000,
+      autoHide: true,
+      fadeInDuration: 500,
+      fadeOutDuration: 500 
+    });
+  }
+
 }
